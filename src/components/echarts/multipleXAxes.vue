abe6cdb1c0371ddc468e611251e564515c68f457
yy-sly <2509756602@qq.com>
2023-08-13 17:56:14 +0800

    asd

---
+++ src/components/echarts/multipleXAxes.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="echarts-box">
+    <div ref="multipleXAxes" :style="{ width: '470px', height: '292px' }"></div>
+  </div>
+</template>
+<script setup>
+import * as echarts from "echarts";
+import { onMounted, onUnmounted, ref } from "vue";
+let echart = echarts;
+const multipleXAxes = ref(null)
+onMounted(() => {
+  initChart();
+});
+
+onUnmounted(() => {
+  echart.dispose;
+});
+// 基础配置一下Echarts
+function initChart() {
+  let chart = echart.init(multipleXAxes.value);
+  // 把配置和数据放这里
+  chart.setOption({
+    tooltip: {
+      trigger: "none",
+      axisPointer: {
+        type: "cross",
+      },
+    },
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        type: "cross",
+        label: {
+          backgroundColor: "#818181",
+        },
+      },
+    },
+    xAxis: {
+      type: "category",
+      boundaryGap: false,
+      data: ["1", "2", "3", "4", "5", "6", "7"],
+      axisLine: {
+        show: false,
+      },
+      axisTick: {
+        show: false,
+      },
+      axisLabel: {
+        textStyle: {
+          color: "#ffffff",
+        },
+      },
+    },
+    yAxis: {
+      type: "value",
+      show: false,
+    },
+    series: [
+      {
+        symbol: "circle", //隐藏点
+        symbolSize: "4",
+        itemStyle: {
+          color: "#676767",
+        },
+        lineStyle: {
+          width: 2,
+          color: "#ffffff",
+        },
+        areaStyle: {
+          normal: {
+            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+              { offset: 0, color: "rgba(250, 250, 250, .3)" },
+              { offset: 1, color: "rgba(250, 250, 250, 0)" },
+            ]),
+          },
+        },
+        data: [320, 216, 318, 428, 234, 361, 421],
+        type: "line",
+        smooth: true,
+      },
+      {
+        symbol: "circle", //隐藏点
+        symbolSize: "4",
+        itemStyle: {
+          color: "#a1a1a1",
+        },
+        lineStyle: {
+          width: 2,
+          color: "#ffffff",
+        },
+        areaStyle: {
+          normal: {
+            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+              { offset: 0, color: "rgba(250, 250, 250, .3)" },
+              { offset: 1, color: "rgba(250, 250, 250, 0)" },
+            ]),
+          },
+        },
+        data: [124, 358, 345, 245, 532, 245, 354],
+        type: "line",
+        smooth: true,
+      },
+      {
+        symbol: "circle", //隐藏点
+        symbolSize: "4",
+        itemStyle: {
+          color: "#393939",
+        },
+        lineStyle: {
+          width: 2,
+          color: "#ffffff",
+        },
+        areaStyle: {
+          normal: {
+            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+              { offset: 0, color: "rgba(250, 250, 250, .3)" },
+              { offset: 1, color: "rgba(250, 250, 250, 0)" },
+            ]),
+          },
+        },
+        data: [253, 425, 352, 125, 352, 145, 253],
+        type: "line",
+        smooth: true,
+      },
+    ],
+  });
+  window.onresize = function () {
+    //自适应大小
+    chart.resize();
+  };
+}
+</script>
+<style lang="scss" scoped>
+.echarts-box {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/views/huoxingViews/homePage/homePage.vue
+++ src/views/huoxingViews/homePage/homePage.vue
@@ -1,36 +1,116 @@
 <template>
-    <div class="appbox">
-        <div class="swiper">
-            <el-carousel height="680px" autoplay>
-                <el-carousel-item v-for="(item, index) in store.state.homeSwiper" :key="index">
-                    <img :src="item.picturePath" alt="">
-                </el-carousel-item>
-            </el-carousel>
-        </div>
-
+  <div class="appbox">
+    <div class="swiper">
+      <el-carousel height="680px" autoplay>
+        <el-carousel-item
+          v-for="(item, index) in store.state.homeSwiper"
+          :key="index"
+        >
+          <img :src="item.picturePath" alt="" />
+          <div class="echarts" v-if="index === 0">
+          <!-- <div class="echarts"> -->
+            <div class="echartsHeader">
+              <h3>今日收益<span>全网</span></h3>
+              <ul class="echartsTable">
+                <li>本月</li>
+                <li>本周</li>
+                <li>今天</li>
+              </ul>
+            </div>
+            <ul class="echartsMessage">
+              <li>
+                <span>84653</span>
+                <span>收益</span>
+              </li>
+              <li>
+                <span>6541</span>
+                <span>收益</span>
+              </li>
+              <li>
+                <span>1324</span>
+                <span>收益</span>
+              </li>
+            </ul>
+            <multipleXAxes />
+          </div>
+        </el-carousel-item>
+      </el-carousel>
     </div>
+  </div>
 </template>
 <script setup>
-import login from "@/components/login/index.js"
-import { useStore } from 'vuex';
+import login from "@/components/login/index.js";
+import multipleXAxes from "@/components/echarts/multipleXAxes.vue";
+import { useStore } from "vuex";
 const store = useStore();
 </script>
 <style lang="scss" scoped>
 .appbox {
+  width: 100%;
+  height: 100%;
+}
+.swiper {
+  width: 1336px;
+  margin: 0 auto;
+  img {
     width: 100%;
     height: 100%;
-}
-.swiper{
-    width: 1336px;
-    margin: 0 auto;
-    img{
+  }
+  .echarts {
+    position: absolute;
+    right: 24px;
+    top: 31px;
+    width: 486px;
+    height: 563px;
+    background: #5d5d5d;
+    .echartsHeader {
+      width: 100%;
+      height: 99px;
+      background: #888888;
+      position: relative;
+      h3 {
         width: 100%;
-        height: 100%;
+        font-size: 30px;
+        font-weight: bold;
+        color: #000000;
+        line-height: 23px;
+        padding-top: 23px;
+        text-align: center;
+      }
     }
+    .echartsTable,
+    .echartsMessage {
+      width: 100%;
+      padding-top: 26px;
+      display: flex;
+      justify-content: space-around;
+      li {
+        font-size: 18px;
+        font-weight: bold;
+        color: #1e1e1e;
+        line-height: 23px;
+      }
+    }
+    .echartsMessage li {
+      width: 121px;
+      height: 121px;
+      background: #afaeae;
+      border: 3px solid #4e4e4e;
+      opacity: 0.3;
+      border-radius: 50%;
+      display: flex;
+      flex-flow: column;
+      justify-content: center;
+      align-items: center;
+      span{
+        color: #1E1E1E;
+      }
+    }
+  }
 }
-:deep(.el-carousel__button){
-    width: 10px;
-    height: 10px;
-    border-radius: 50%;
+:deep(.el-carousel__button) {
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
 }
 </style>
