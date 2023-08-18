886769526dd601bb3c7d57491145bb9398e83115
yy-sly <2509756602@qq.com>
2023-08-18 11:40:24 +0800

    asd

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath:"./",  // 可以设置成相对路径，这样所有的资源都会被链接为相对路径，打出来的包可以被部署在任意路径
+  publicPath: "./",  // 可以设置成相对路径，这样所有的资源都会被链接为相对路径，打出来的包可以被部署在任意路径
+  pwa: {
+    iconPaths: {
+      favicon32: './icon.png',
+      favicon16: './icon.png',
+      appleTouchIcon: './icon.png',
+      maskIcon: './icon.png',
+      msTileImage: './icon.png'
+    }
+  }
 })
